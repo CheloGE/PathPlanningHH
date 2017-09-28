dc42da5de8c824ac28657de36863646f38dd0355
Marcelo <chelo_ge@hotmail.com>
2017-09-28 17:17:49 +0100

    added planner, road, vehicle, map, spline and utils

---
+++ src/map.h
@@ -0,0 +1,36 @@
+#ifndef MAP_H
+#define MAP_H
+
+#include <string>
+#include <vector>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <math.h>
+
+#include "spline.h"
+
+//using namespace std;
+//using namespace tk; // spline
+namespace{
+class Map {
+
+  protected:
+  // define wp spline trajectory
+    tk::spline wp_spline_x;
+    tk::spline wp_spline_y;
+    tk::spline wp_spline_dx;
+    tk::spline wp_spline_dy;
+    
+
+  public:
+    
+    Map(){};
+    Map(std::string map_file_);
+    ~Map() {};
+
+    std::vector<double> getXY(double s, double d);
+
+};
+}
+#endif // ROAD_H
